96a23f949e932a971a80e3a3e52437aca58a348e
aNIMESHDEV <animesh@esoftech.in>
2020-06-06 02:42:19 +0530

    settings changes

--- customer/models.py
+++ customer/models.py
@@ -9,12 +9,12 @@ class Customers(models.Model):
     first_name = models.CharField(max_length=128)
     last_name = models.CharField(max_length=128)
     email = models.CharField(max_length=128)
-    mobile = models.IntegrFiels(max_length=128)
+    mobile = models.IntegerField(max_length=128)
     address1 = models.CharField(max_length=128)
     address2 = models.CharField(max_length=128)
     address3 = models.CharField(max_length=128)
     address4 = models.CharField(max_length=128)
-    zip = models.IntegerField(max_length=128)
+    zip = models.BigIntegerField(max_length=128)
     city = models.CharField(max_length=128)
     state = models.CharField(max_length=128)
     country = models.CharField(max_length=128)
@@ -26,7 +26,7 @@ class ref_payment_methods(models.Model):
     payment_method_des = models.CharField(max_length=256)
 
 class cust_payments_methods(models.Model):
-    cust_payment_id = models.Autofield(primary_key=True)
+    cust_payment_id = models.AutoField(primary_key=True)
     customer = models.ForeignKey(Customers,on_delete=models.CASCADE)
     payment_method_code = models.ForeignKey(ref_payment_methods,on_delete=models.CASCADE)
     card_number = models.CharField(max_length=24)

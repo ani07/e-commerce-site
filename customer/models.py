441a3c28921edbcf2112d3d946f0c0209208a1af
rajesh-n-j <rajeshjha9211@gmail.com>
2020-06-06 05:00:23 +0530

    customer table

--- customer/models.py
+++ customer/models.py
@@ -9,12 +9,12 @@ class Customers(models.Model):
     gender = models.CharField(max_length=2)
     dob = models.DateField()
     email = models.CharField(max_length=128)
-    mobile = models.IntegerField(max_length=128)
+    mobile = models.IntegerField()
     address1 = models.CharField(max_length=128)
     address2 = models.CharField(max_length=128)
     address3 = models.CharField(max_length=128)
     address4 = models.CharField(max_length=128)
-    zip_code = models.IntegerField(max_length=128)
+    zip_code = models.IntegerField()
     city = models.CharField(max_length=128)
     state = models.CharField(max_length=128)
     create_date = models.DateTimeField(blank=True, null=True)
@@ -22,19 +22,26 @@ class Customers(models.Model):
     country = models.CharField(max_length=128)
     auth_user_id = models.OneToOneField(AuthUser, models.DO_NOTHING, db_column="auth_user_id", related_name='customers')
     class Meta:
-        managed = False
+        managed = True
+        db_table = "Customers"
 
     
 
-# class ref_payment_methods(models.Model):
-#     payment_method_code = models.IntegerField(primary_key=True)
-#     payment_method_des = models.CharField(max_length=256)
+class ref_payment_methods(models.Model):
+    payment_method_code = models.IntegerField(primary_key=True)
+    payment_method_des = models.CharField(max_length=256)
+    class Meta:
+        managed = True
+        db_table = "Ref_payment_methods"
 
-# class cust_payments_methods(models.Model):
-#     cust_payment_id = models.IntegerField(primary_key=True)
-#     customer = models.ForeignKey(Customers,on_delete=models.CASCADE)
-#     payment_method_code = models.ForeignKey(ref_payment_methods,on_delete=models.CASCADE)
-#     card_number = models.CharField(max_length=24)
-#     name_on_card = models.CharField(max_length=256)
-#     expiry_date = models.DateField(auto_now=False)
-#     other_details = models.CharField(max_length=256)
+class cust_payments_methods(models.Model):
+    cust_payment_id = models.IntegerField(primary_key=True)
+    customer = models.ForeignKey(Customers,on_delete=models.CASCADE)
+    payment_method_code = models.ForeignKey(ref_payment_methods,on_delete=models.CASCADE)
+    card_number = models.CharField(max_length=24)
+    name_on_card = models.CharField(max_length=256)
+    expiry_date = models.DateField(auto_now=False)
+    other_details = models.CharField(max_length=256)
+    class Meta:
+        managed = True
+        db_table = "Cust_Payments_Methods"
